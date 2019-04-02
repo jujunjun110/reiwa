5bdc6d2b34c1189d7234cef6a3fb3b1b046cb96b
Jun Ito <jun_ito@voyagegroup.com>
2019-04-02 15:29:44 +0900

    Update: 綺麗に動くようにした

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,51 @@
-import MyMod from './js/my_mod'
+import 'aframe'
 import './scss/style.scss'
+declare var AFRAME: any;
 
-async function main(): Promise<void> {
-  const m = new MyMod()
+function main(): void {
+  AFRAME.registerComponent('time-counter', {
+    schema: {},
+    init: function () {
+      this.data.count = 0
+    },
+    tick: function () {
+      this.data.count += 1
+      this.el.setAttribute('material', 'time', this.data.count * 0.01)
+    }
+  })
+  AFRAME.registerShader('time-gradation-shader', {
+    schema: {
+      time: { type: 'float', default: 0.0, is: 'uniform' }
+    },
+    vertexShader: [
+      'varying vec2 vUV;',
+      'uniform float time;',
+      '',
+      'void main(void) {',
+      '    //position: vec3([-0.5 ~ 0.5], [-0.5 ~ 0.5], [-0.5 ~ 0.5])',
+      '    float Pi = 3.141592;',
+      '',
+      '    float tx = position.x * (abs(sin(position.y * Pi + time)) * 0.4 + 0.6);',
+      '    float ty = position.y;',
+      '    float tz = position.z * (abs(sin(position.y * Pi + time)) * 0.4 + 0.61);',
+      '',
+      '    vec3 transform = vec3(tx, ty, tz);',
+      '',
+      '    gl_Position = projectionMatrix * modelViewMatrix * vec4(transform, 1.0);',
+      '    vUV = uv;',
+      '}',
+    ].join("\n"),
+    fragmentShader: [
+      'varying vec2 vUV; // [0.0, 0.0] ~ [1.0, 1.0]',
+      'uniform float time;',
+      'void main(void) {',
+      '  vec2 p = (vUV * 2.0) - vec2(1.0, 1.0); // [-1.0, 1.0] ~ [1.0, 1.0]',
+      '  float x = p[0];',
+      '  float y = p[1];',
+      '  gl_FragColor = vec4(sin(time * 3.0) * 0.5 + 0.5, sin(time * 2.0) * 0.5 + 0.5, sin(time) * 0.5 + 0.5, 0.9); //(Red, Green, Blue, Alpha)',
+      '}',
+    ].join("\n"),
+  })
 }
 
 main()
